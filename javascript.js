81707d1d9a6e6b59a12789af35c14c13d2cfad2b
xpGui <90066099+xpGui@users.noreply.github.com>
2024-03-14 01:20:53 -0300

    Try to add loop to the code
    And failing. lol.

--- javascript.js
+++ javascript.js
@@ -1,40 +1,63 @@
-// NEED TO RESUME PROJECT FROM PART 6
+// IDK WHAT IS HAPPENING :(
 
-function getComputerChoice() {
-    let choice = Math.floor(Math.random() * (3-1+1) + 1);
-    switch(choice) {
-        case 1:
-            return "rock";
-        case 2:
-            return "paper";
-        case 3:
-            return "scissors";
-        default:
-            return "nothing";
+function playGame() {
+    let computerPoints = 0;
+    let playerPoints = 0;
+
+    while (computerPoints || playerPoints != 5) {
+
+        function getComputerChoice() {
+            let choice = Math.floor(Math.random() * (3-1+1) + 1);
+            switch(choice) {
+                case 1:
+                    return "rock";
+                case 2:
+                    return "paper";
+                case 3:
+                    return "scissors";
+                default:
+                    return "nothing";
+            }
+        }
+
+        function playRound(playerSelection, computerSelection) {
+            switch(true) {
+                case playerSelection.toLowerCase() === computerSelection:
+                    return "Draw! Both players chose " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + "!";
+                case playerSelection.toLowerCase() === 'rock' && computerSelection == "paper":
+                    computerPoints += 1
+                    return "You Lose! Paper beats Rock!";
+                case playerSelection.toLowerCase() === 'rock' && computerSelection == "scissors":
+                    playerPoints +=1
+                    return "You Win! Rock beats Scissors!";
+                case playerSelection.toLowerCase() === 'paper' && computerSelection == "rock":
+                    playerPoints +=1
+                    return "You Win! Paper beats Rock!";
+                case playerSelection.toLowerCase() === 'paper' && computerSelection == "scissors":
+                    computerPoints += 1
+                    return "You Lose! Scissors beats Paper!";
+                case playerSelection.toLowerCase() === 'scissors' && computerSelection == "paper":
+                    playerPoints +=1
+                    return "You Win! Scissors beats Paper!";
+                case playerSelection.toLowerCase() === 'scissors' && computerSelection == "rock":
+                    computerPoints += 1
+                    return "You Lose! Rock beats Scissors!";
+                default:
+                    null;
+            }
+        }
+
+        const computerSelection = getComputerChoice();
+        const playerSelection = prompt("What's your play?");
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("Computer has " + computerPoints + " points!");
+        console.log("Player has " + playerPoints + " points!");
+
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    switch(true) {
-        case playerSelection.toLowerCase() === computerSelection:
-            return "Draw! Both players chose " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + "!";
-        case playerSelection.toLowerCase() === 'rock' && computerSelection == "paper":
-            return "You Lose! Paper beats Rock!";
-        case playerSelection.toLowerCase() === 'rock' && computerSelection == "scissors":
-            return "You Win! Rock beats Scissors!";
-        case playerSelection.toLowerCase() === 'paper' && computerSelection == "rock":
-            return "You Win! Paper beats Rock!";
-        case playerSelection.toLowerCase() === 'paper' && computerSelection == "scissors":
-            return "You Lose! Scissors beats Paper!";
-        case playerSelection.toLowerCase() === 'scissors' && computerSelection == "paper":
-            return "You Win! Scissors beats Paper!";
-        case playerSelection.toLowerCase() === 'scissors' && computerSelection == "rock":
-            return "You Lose! Rock beats Scissors!";
-        default:
-            null;
-    }
-}
+playGame();
 
-const computerSelection = getComputerChoice();
-const playerSelection = "pApER";
-console.log(playRound(playerSelection, computerSelection));
+
+
+
